521d05a18eeed5701255d288da53940da12d848c
Serban Andrei <reiserban@gmail.com>
2026-02-18 01:18:22 +0100

    Andre

--- scripts/main.js
+++ scripts/main.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
     "about": {
       preview: "About me & about work",
       full: `<h2></h2>
-        <p>Andrei Serban (b.1999, Bucharest)<br><br><em>Royal Academy of Arts The Hague - Photography 2028</em><br><em>Central Saint Martins - Fashion Image 2022</em>
+        <p>Andre Serban (b.1999, Bucharest)<br><br><em>Royal Academy of Arts The Hague - Photography 2028</em><br><em>Central Saint Martins - Fashion Image 2022</em>
         <br><br><br>
         <br> Table for 1(1) - Group Show - Den Haag
         <br> 2/3 Galeria - CHIPS Exhibition & Book Launch - March 2024<br><br>
